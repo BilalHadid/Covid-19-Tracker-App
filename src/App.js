53a5622d2cf8543a03e142039e06bf3ffbec46d2
bilalhadid <hadid0648@gmail.com>
2020-06-17 01:47:20 +0500

    Add chart.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button'
+import {Chart} from './component/chart'
 
 function App() {
+  
+
   return (
     <div className="App">
       <Button variant="contained" color="primary">Hello World</Button>
+      <Chart />
     </div>
   );
 }
---
+++ src/component/chart.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Bar, Line, Pie} from 'react-chartjs-2'
+
+export const Chart = () => {
+    const state = {
+        charData:{
+            labels:['Bilal','hadid','Fajruddin','Qasim','usama','suhaib'],
+            datasets:[
+                {
+                    label: 'Population',
+                    data:[
+                        617594,
+                        184145,
+                        389256,
+                        867523,
+                        124586,
+                        856758
+
+                    ],
+                    backgroundColor:[
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 253, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)',
+                    ]
+                }
+            ]
+        }
+    }
+    return(
+        <div className="chart">
+            <Bar
+                data={state.charData}
+                
+                options={{
+                }} />
+
+        </div>
+    )
+}
