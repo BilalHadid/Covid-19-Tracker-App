cde17737b474c30bdff4bb7f51ba636c78146ecc
bilalhadid <hadid0648@gmail.com>
2020-06-21 23:24:38 +0500

    Add Card

--- src/component/card/card.js
+++ src/component/card/card.js
@@ -1,9 +1,32 @@
 import React  from 'react'
-import Box from '@material-ui/core/Box';
-import {Button} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles";
+import Cards from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Icon from "@material-ui/core/Icon";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      background: "#4D26B7"
+    },
+    bullet: {
+      display: "inline-block",
+      margin: "0 2px",
+      transform: "scale(0.8)"
+    },
+    title: {
+      fontSize: 14
+    }
+  });
 
 export const Card = ({finalize, currentDate}) => {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+  
     return(
+        
         <div>
             <div>
             <h1>New Confirmed</h1>
@@ -23,9 +46,7 @@ export const Card = ({finalize, currentDate}) => {
                 <h1>Current Date</h1>
                 <h2>{new Date(currentDate).toDateString()}</h2>
             </div>
-            <Box component="span" m={1}>
-                    <Button color="primary"/>
-            </Box>
+            
         </div>
     )
 }   
